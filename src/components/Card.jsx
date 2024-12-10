2a5c12bf3981a109017a92362de87bed3eac87b7
DiegoAaronMP <daaron.macias@gmail.com>
2024-12-10 17:59:31 -0600

    Cambiado el ancho de la imagen al 70%

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,9 @@
 export const Card = () => {
   return (
     <div className='bg-white rounded-md justify-items-center'>
-        <div >
-            <img className='max-w-full' src='src/assets/25.png' alt="Pokémon artwork" />
-        </div>
-        
+       
+        <img className='w-[70%]' src='src/assets/25.png' alt="Pokémon artwork" />
+       
         <footer className='bg-red-500 w-full rounded-b-md'>
             <p className='text-base text-white font-semibold'>#025 Pikachu</p>
         </footer>
