7536234992fb3f41d79d89d02ef0d630eb0dc616
DiegoAaronMP <daaron.macias@gmail.com>
2024-12-11 16:23:22 -0600

    Agregado un badge indicando el número de la Pokédex

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,16 @@
 export const Card = () => {
   return (
     <div className='bg-white rounded-md justify-items-center'>
-       
-        <img className='w-[70%]' src='src/assets/25.png' alt="Pokémon artwork" />
-       
-        <footer className='bg-red-500 w-full rounded-b-md'>
-            <p className='text-base text-white font-semibold'>#025 Pikachu</p>
-        </footer>
+      {/* Pokédex number */}
+      <span className="absolute px-1.5 m-1 rounded-lg text-xs font-extrabold text-gray-700 bg-gray-300">
+        #025
+      </span>
+
+      <img className='w-[70%]' src='src/assets/25.png' alt="Pokémon artwork" />
+
+      <footer className='bg-red-500 w-full rounded-b-md'>
+        <p className='text-base text-white font-semibold'>#025 Pikachu</p>
+      </footer>
     </div>
   )
 }
