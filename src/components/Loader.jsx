be86f0dbbdd17b6935c77f801e8e1d03f121d657
DiegoAaronMP <daaron.macias@gmail.com>
2024-12-17 12:41:46 -0600

    Creado un spinner con clases de CSS

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,9 @@
 
 export const Loader = () => {
   return (
-    <div>Loading...</div>
+    <div className='text-center mb-4'>
+      <div className='spinner mx-auto'></div>
+      <p className='font-bold mt-1.5'>Loading</p>
+    </div>
   )
 }
