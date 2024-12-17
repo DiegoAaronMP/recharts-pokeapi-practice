156ad246ea53fefe28b5f502a75375372925fdff
DiegoAaronMP <daaron.macias@gmail.com>
2024-12-16 18:21:14 -0600

    Agregado el componente Loader a HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,21 @@
-import { Card } from "../components/Card"
-import { useGetPokemon } from "../hooks/useGetPokemon";
+import { Card } from '../components/Card'
+import { Loader } from '../components/Loader';
+import { useGetPokemon } from '../hooks/useGetPokemon';
 
 export const HomePage = () => {
     const { pokemon } = useGetPokemon();
     
     return (
+        <>
+            <div className='grid grid-cols-cards gap-4 m-6 md:mx-36'>
+                {
+                    pokemon?.map(poke =>
+                        <Card key={poke.id} {...poke} />
+                    )
+                }
+            </div>
 
-        <div className="grid grid-cols-cards gap-4 m-6 md:mx-36">
-            {
-                pokemon?.map(poke =>
-                    <Card key={poke.id} {...poke} />
-                )
-            }
-        </div>
-
+            <Loader />
+        </>
     )
 }
