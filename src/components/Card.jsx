166824cd70c190971b5a4abd7a143d94d7878a8a
DiegoAaronMP <daaron.macias@gmail.com>
2024-12-18 17:09:47 -0600

    Agregado un margen alrededor de la imagen del Pokémon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ export const Card = ({ id, name, sprite }) => {
   return (
     <div className='bg-white rounded-md ring-1 ring-gray-200 shadow-sm hover:scale-110 transition'>
 
-      <div className="justify-items-center items-center justify-between px-2.5 py-1.5">
+      <div className='justify-items-center items-center justify-between px-2.5 py-1.5'>
 
-        <img className='w-full h-20 max-w-20 z-10' src={sprite} alt="Pokémon artwork" />
+        <img className='w-full h-20 max-w-20 m-2 z-10' src={sprite} alt='Pokémon artwork' />
 
-        <span className="px-2 rounded-lg tracking-wide text-xs font-extrabold text-gray-800 bg-gray-200">
+        <span className='px-2 rounded-lg tracking-wide text-xs font-extrabold text-gray-800 bg-gray-200'>
           #{id}
         </span>
         
