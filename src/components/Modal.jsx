6039ea16357e9978ac31abfca19eb2a0add5e976
Diego <daaron.macias@gmail.com>
2025-03-24 11:56:36 -0600

    Implementación de prueba de la gráfica de barras

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
-export const Modal = ({open, activePokemon, close}) => {
+export const Modal = ({ open, activePokemon, close }) => {
 
   // Cerrar el modal cuando se presiona la tecla Esc
   useEffect(() => {
@@ -15,19 +16,32 @@ export const Modal = ({open, activePokemon, close}) => {
     // Eliminar el listener cuando el modal se cierra
     return () => document.removeEventListener('keydown', handleKeyDown);
   }, [close]);
-  
+
 
   return (
     <div onClick={close} className={`fixed inset-0 bg-black/30 flex justify-center items-center ${open ? '' : 'hidden'}`}>
-        {/* El stopPropagation sirve para que el evento onClick del padre no llegue a los hijos,
+      {/* El stopPropagation sirve para que el evento onClick del padre no llegue a los hijos,
             en este caso se está utilizando para evitar que se cierre el modal al dar clic dentro
             del contenido/hijo */}
-        <div onClick={(e) => e.stopPropagation()} className="flex bg-white rounded-md w-full max-w-3xl max-h-full p-6 mx-4">
-            <div>
-              <h2 className="font-bold text-2xl uppercase">{activePokemon.name}</h2>
-              <img className='w-24 h-auto mt-4 mb-2 z-10' src={activePokemon.sprite} alt={`${activePokemon.name} artwork`} />
-            </div>
+      <div onClick={(e) => e.stopPropagation()} className="flex bg-white rounded-md w-full max-w-3xl max-h-full p-6 mx-4">
+        <div>
+          <h2 className="font-bold text-2xl uppercase">{activePokemon.name}</h2>
+          <img className='w-24 h-auto mt-4 mb-2 z-10' src={activePokemon.sprite} alt={`${activePokemon.name} artwork`} />
         </div>
+
+        {/* Gráfica de barras */}
+        <ResponsiveContainer width={'100%'} height={300} >
+          <BarChart
+            data={activePokemon.stats}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="base" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   )
 }
