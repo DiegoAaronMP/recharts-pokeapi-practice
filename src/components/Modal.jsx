fa9e15d2bc703ada52b9a45010bdb57004f4a359
Diego <daaron.macias@gmail.com>
2025-03-26 12:20:19 -0600

    Implementada funcionalidad para deshabilitar el botón seleccionado

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -64,14 +64,16 @@ export const Modal = ({ open, activePokemon, close }) => {
         {/* Contenedor para los botones */}
         <div className="flex gap-2">
           <button 
-            className="bg-blue-700 py-2 px-4 text-white rounded-md shadow-md hover:bg-blue-800 focus:ring-4 focus:ring-blue-900"
+            className={`bg-blue-700 py-2 px-4 text-white rounded-md shadow-md hover:bg-blue-800 focus:ring-4 focus:ring-blue-900 ${selectedChart === 'bar' ? 'opacity-85' : ''}`}
             onClick={() => setSelectedChart('bar')}
+            disabled={selectedChart === 'bar'}
           >
             Bar Chart
           </button>
           <button 
-            className="bg-blue-700 py-2 px-4 text-white rounded-md shadow-md hover:bg-blue-800 focus:ring-4 focus:ring-blue-900"
+            className={`bg-blue-700 py-2 px-4 text-white rounded-md shadow-md hover:bg-blue-800 focus:ring-4 focus:ring-blue-900 ${selectedChart === 'radar' ? 'opacity-85' : ''}`}
             onClick={() => setSelectedChart('radar')}
+            disabled={selectedChart === 'radar'}
           >
             Radar Chart
           </button>
